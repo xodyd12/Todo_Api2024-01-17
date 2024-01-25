7dfe2ec8485816fae52e8ab12073c0b4f68f2891
Hantaeyong <hhhhgong3@naver.com>
2024-01-25 11:19:42 +0900

    #  삭제에도 양방향 넣기

--- src/main/java/com/study/todoapi/todo/service/TodoService.java
+++ src/main/java/com/study/todoapi/todo/service/TodoService.java
@@ -68,7 +68,12 @@ public class TodoService {
     public TodoListResponseDTO delete(String id, String email) {
 
         try {
-            todoRepository.deleteById(id);
+            Todo todo = todoRepository.findById(id).orElseThrow();
+
+            //양방향에서는 반대편 리스트는 수동으로 갱신해야 함.
+            User user = userRepository.findByEmail(email).orElseThrow();
+            user.getTodoList().remove(todo);
+
         } catch (Exception e) {
             log.error("id가 존재하지 않아 삭제에 실패했습니다. - ID: {}, error: {}",
                     id, e.getMessage());
